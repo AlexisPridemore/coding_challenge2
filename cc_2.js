460e8742bd36bee374e5a084b571c4de8dd118d0
Alexis Pridemore <alpridemore@usf.edu>
2025-01-26 15:05:34 -0500

    Task 1-Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,12 @@
+// Task 1 Working with Arrays
+
+let products = ["phone", "laptop", "airpods", "speaker", "tablet"]; //Array of products
+
+products.push("charger");  //Add new product to the end of the array
+
+console.log(products);    //Print the Array of products
+
+products.pop();     //Remove the last product from the array
+
+console.log("Updated products:", products);   //Print the array of products
+
